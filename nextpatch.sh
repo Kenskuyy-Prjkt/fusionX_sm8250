a57ec628b1b82d6364e266874fc5b2c3c0c97b2d
SenseiiX <senseixdev@gmail.com>
2025-10-26 12:32:14 +0800

    nextpatch: Use my KSUN fork
    Signed-off-by: SenseiiX <senseixdev@gmail.com>

--- nextpatch.sh
+++ nextpatch.sh
@@ -1,4 +1,4 @@
 rm -rf KernelSU-Next
-curl -LSs "https://raw.githubusercontent.com/re-noroi/KernelSU-Next/susfs/kernel/setup.sh" | bash -s susfs
+curl -LSs "https://raw.githubusercontent.com/SenseiiX/KernelSU-Next/susfs/kernel/setup.sh" | bash -s susfs
 echo "Adding more manager support..."
 echo "Done (KSU, MKSU, RKSU, Fork by backslashxx, SukiSU and WKSU)"
