446b6a15aeab38c1c0eebf46dba6a982e7a5b0f3
SenseiiX <senseixdev@gmail.com>
2025-11-22 19:45:29 +0800

    Revert "proc: fake all lineage symlinks"
    This reverts commit a761a61d88bb609b376274db6847425d8afe415b.
    
    Change-Id: Ie893726a66df09870d01a8966256913e5f35c0d4
    Signed-off-by: SenseiiX <senseixdev@gmail.com>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -397,18 +397,6 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma)
 bypass_orig_flow:
 #endif
 		pgoff = ((loff_t)vma->vm_pgoff) << PAGE_SHIFT;
-		dentry = file->f_path.dentry;
-        if (dentry) {
-        	const char *path = (const char *)dentry->d_name.name; 
-            	if (strstr(path, "lineage") ||
-					strstr(path, "lineageos")) { 
-	            	start = vma->vm_start;
-	             	end = vma->vm_end;
-	             	show_vma_header_prefix_fake(m, start, end, flags, pgoff, dev, ino);
-                	name = "/system/framework/framework-res.apk";
-		            goto done;
-            	}
-            }
 	}
 
 	start = vma->vm_start;
